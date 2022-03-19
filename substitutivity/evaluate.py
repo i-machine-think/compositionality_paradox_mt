16f7036acd9315e7bb52b970f8f929e2a1bbc63c
Verna <vernadankers@gmail.com>
2022-03-19 12:02:50 +0000

    Camera ready :-)

--- substitutivity/evaluate.py
+++ substitutivity/evaluate.py
@@ -1,15 +1,26 @@
 import pickle
 import os
+import random
 import numpy as np
+
+
+def set_seed(seed):
+    """Set random seed."""
+    if seed == -1:
+        seed = random.randint(0, 1000)
+    np.random.seed(seed)
+    random.seed(seed)
 
 
 def reorder(lines, check=True, num_lines=500):
     """
     Takes the shuffled Fairseq translations and puts them back in order.
+
     Args:
         lines: list of strings, Fairseq output
         check: whether to check the file length
         num_lines: the expected number of lines
+
     Returns:
         lines: list of strings, reordered
     """
@@ -27,7 +38,6 @@ def reorder(lines, check=True, num_lines=500):
 
 
 def transform(num, synonym_translations, sent, noun_only):
-
     translations = []
     for w in synonym_translations[num]:
         sent = sent.replace(w, w.replace(" ", "_"))
@@ -35,21 +45,20 @@ def transform(num, synonym_translations, sent, noun_only):
 
     if not noun_only:
         return sent
-    nl = None
+
+    crop = sent
     for n in translations:
-        if n in sent:
-            nl = n
-            break
-    if nl is None:
-        nl = translations[0]
-    sent = [x if nl not in x else nl for x in sent.split()]
-    try:
-        return sent[sent.index(nl)]
-    except:
-        return " ".join(sent)
+        for w in sent.split():
+            if n in w.lower():
+                crop = w
+                return crop
+    return crop
 
 
-def consistency(prefix, synonym_num, synonym_data, noun_only, check=True):
+def consistency(src_prefix, prefix, synonym_num, synonym_data, noun_only, check=True):
+    with open(f"{src_prefix}-1.en", encoding="utf-8") as f:
+        srcs = f.readlines()
+    trace = []
     with open(f"{prefix}-1.nl", encoding="utf-8") as f:
         british_lines = reorder(f.readlines(), check)
         british_lines = [transform(synonym_num, synonym_data, l, noun_only) for l in british_lines]
@@ -57,12 +66,15 @@ def consistency(prefix, synonym_num, synonym_data, noun_only, check=True):
         american_lines = reorder(f.readlines(), check)
         american_lines = [transform(synonym_num, synonym_data, l, noun_only) for l in american_lines]
     scores = []
-    for x, y in zip(british_lines, american_lines):
+    for s, x, y in zip(srcs, british_lines, american_lines):
         scores.append(x.strip() == y.strip())
-    return np.mean(scores)
+        if x.strip() != y.strip():
+            trace.append("\t".join([s.strip(), x.strip(), y.strip()]))
+    return np.mean(scores), trace
 
 
 if __name__ == "__main__":
+    set_seed(1)
 
     # Collect synonyms and their potential translations from file
     assert os.path.exists("synonyms.tsv"), "Please collect synonym data first."
@@ -70,10 +82,10 @@ if __name__ == "__main__":
     with open("synonyms.tsv", encoding="utf-8") as f:
         f.readline()
         for i, line in enumerate(f):
-            synonym1, synonym2, _, _, _, translations1, translations2 = \
+            synonym1, synonym2, translation, _, _, translations1, translations2 = \
                 line.strip().split("\t")
-            translations = set(translations1.split(';') + translations2.split(';'))
-            synonyms[i] = translations
+            translations = list(set(translations1.split(';') + translations2.split(';')))
+            synonyms[i] = [translation] + translations
 
     # Collect numerical results in a dictionary that will be visualised in
     # a separate jupyter notebook
@@ -84,12 +96,23 @@ if __name__ == "__main__":
                 for template in range(1, 11):
                     for syn_num in range(20):
                         print(model, seed, data_type, template, syn_num)
+
+                        if data_type != "natural":
+                            src_prefix = f"{data_type}/substitutivity_{data_type}_{template}/{syn_num}"
+                        else:
+                            src_prefix = f"{data_type}/{syn_num}"
+
                         prefix = f"pred_{data_type}/transformer_{model}_{seed}/"
                         if data_type != "natural":
                             prefix += f"substitutivity_{data_type}_{template}_"
-                            
-                        cons = consistency(f"{prefix}{syn_num}", syn_num, synonyms, noun_only=False, check=data_type != "natural")
-                        cons_natural = consistency(f"{prefix}{syn_num}", syn_num, synonyms, noun_only=True, check=data_type != "natural")
+
+                        cons, trace = consistency(f"{src_prefix}", f"{prefix}{syn_num}", syn_num, synonyms, noun_only=False, check=data_type != "natural")
+                        cons_natural, _ = consistency(f"{src_prefix}", f"{prefix}{syn_num}", syn_num, synonyms, noun_only=True, check=data_type != "natural")
+                        with open(f"traces/model={model}_data={data_type}_template={template}_synonym={syn_num}_seed={seed}.tsv", 'w') as f:
+                            f.write("source\ttranslation_syn1\ttranslation_syn2\n")
+                            for line in trace:
+                                f.write(line.strip() + "\n")
+
                         if data_type == "natural":
                             results[(model, seed, data_type, syn_num)] = (cons, cons_natural)
                         else:
